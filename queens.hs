35cb29a38a2f7fac6f06b694c4f1baf59c83ae0b
Mateu Adsuara <mateuadsuara@gmail.com>
2021-09-27 20:20:20 +0200

    Complete queens problem

--- queens.hs
+++ queens.hs
@@ -1,4 +1,7 @@
 -- import Data.Functor.Foldable (hylo)
+{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveFunctor, RecordWildCards #-}
+
+import Control.Monad (guard)
 
 type Alg f e = f e -> e
 type CoAlg f e = e -> f e
@@ -6,6 +9,7 @@ type CoAlg f e = e -> f e
 hylo :: Functor f => Alg f b -> CoAlg f a -> a -> b
 hylo alg coalg = alg . fmap h . coalg
      where h = hylo alg coalg
+
 
 --8                   Q    |
 --7          Q             |
@@ -16,8 +20,45 @@ hylo alg coalg = alg . fmap h . coalg
 --2             X  Q       |
 --1 Q                      |
 
+--  1  2  3  4  5  6  7  8
+--  Q
+--        Q
+--              Q
+--                    Q
+--           Q
+--     Q
+--                       Q
+--                 Q
+
 -- [1, 3, 5, 7, 4, 2, 8, 6]
 
+
+
+data QueensF r = Done [Int] | Node [r] deriving (Functor, Show)
+
+newtype Problem = Problem { previousQueenPositions :: [Int] } deriving Show
+
+coAlg :: CoAlg QueensF Problem
+coAlg Problem{..} = if length previousQueenPositions == 8 then
+                      Done $ previousQueenPositions
+                    else
+                      Node $ do
+                        possiblePosition <- [1..8]
+                        guard $ isAvailableRow previousQueenPositions possiblePosition
+                        return $ Problem (previousQueenPositions ++ [possiblePosition])
+                        --[ Problem (previousQueenPositions ++ [possiblePosition]) |
+                        --  possiblePosition <- [1..8], isAvailableRow previousQueenPositions possiblePosition
+                        --]
+
+type Solutions = [[Int]]
+
+
+alg :: Alg QueensF Solutions
+alg (Done solution) = [solution]
+alg (Node severalSolutions) = concat severalSolutions
+
+eightQueens :: Solutions
+eightQueens = hylo alg coAlg $ Problem []
 
 type UsedRows = [Int]
 
@@ -31,5 +72,5 @@ isAvailableRow rs r = let current = (r, length rs + 1)
                           ok prev = not (sameDiag prev current) && not (sameRow prev current)
                       in all ok previous
 
-queens :: [Int]
-queens = undefined
+showQueens :: [Int] -> String
+showQueens queens = unlines $ " 12345678" : map (\q-> replicate q ' ' ++ "Q") queens
